4be35b5fafd906f08999179e3f369d1a6525471d
MusthakSM <musthakahamed741300@gmail.com>
2024-11-25 17:51:30 +0530

    Extracting the JSX into a new component

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -129,10 +129,7 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           Currently we are close.
@@ -141,6 +138,17 @@ function Footer() {
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
