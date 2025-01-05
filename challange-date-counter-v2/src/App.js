fe0f78f058c9d233c159d5258993fa00b187af68
MusthakSM <musthakahamed741300@gmail.com>
2025-01-05 11:52:11 +0530

    date counter challange 2 has been completed

---
+++ challange-date-counter-v2/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setStep(1);
+    setCount(0);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          value={step}
+          min={0}
+          max={10}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((count) => count - step)}>-</button>
+        <input type="text" value={count} />
+        <button onClick={() => setCount((count) => count + step)}>+</button>
+      </div>
+
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
