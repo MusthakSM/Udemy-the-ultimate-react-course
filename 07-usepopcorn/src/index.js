1943d6a8671a554cc0fec6f58c2c7a1b13eb73e2
MusthakSM <musthakahamed741300@gmail.com>
2025-02-28 07:44:19 +0530

    Made the StarRating components' props as a component API, provided many options for modifications for user

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This move has been rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["hello", "cool", "awesome", "horrible", "nice"]}
+    />
+    <StarRating maxRating={10} size={24} color="red" defaultRating={5} />
+
+    <Test />
   </React.StrictMode>
 );
