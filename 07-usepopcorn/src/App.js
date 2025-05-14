f400a77d06b902852484ae9cf815a4419db9292d
MusthakSM <musthakahamed741300@gmail.com>
2025-05-15 04:17:51 +0530

    select an element using useRef instead of using document.querySelector

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./index.css";
 import "./StarRating";
 import StarRating from "./StarRating";
@@ -158,6 +158,33 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
+  // This is not the way to select DOM elements in React. Though it's possible, better to go with useRef hook.
+  // useEffect(function () {
+  //   const el = document.querySelector(".search");
+  //   el.focus();
+  // }, []);
+
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) {
+          return;
+        }
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -165,6 +192,7 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
